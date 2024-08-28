8c46d92ab373332b01eaee80cf488020a97d7ca5
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-08-28 01:40:08 +0100

    add zod and react hook form for login validation

--- frontend/src/Components/LogIn.tsx
+++ frontend/src/Components/LogIn.tsx
@@ -1,49 +1,88 @@
-import React from "react";
+import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email",
+  }),
+  password: z.string().min(8, { message: "Must be at least 8 characters" }),
+});
+
+type UserDataType = z.infer<typeof loginSchema>;
 
 export default function LogIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Form submitted");
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<UserDataType>({
+    defaultValues: {
+      email: "test@email.com",
+      password: "test",
+    },
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit: SubmitHandler<UserDataType> = async (data) => {
+    try {
+      throw new Error("tf is happening");
+    } catch (error) {
+      setError("root", {
+        message: "tf is happening",
+      });
+      console.log(error);
+    }
   };
 
+
   return (
-    <section className="bg-blue-100 flex flex-col items-center min-h-dvh">
+    <section
+      id="login"
+      className="bg-blue-100 flex flex-col items-center min-h-dvh"
+    >
       <h1 className="text-5xl font-semibold text-gray-700 mt-12">
         Dear Diary...
       </h1>
       <p className="mt-4 text-md text-gray-800 ">
         Your safe space to journal and self reflect
       </p>
-      <div className="flex flex-col justify-center items-center bg-white rounded-2xl shadow-xl p-12 h-[350px] mt-32">
+      <div className="flex flex-col justify-center items-center bg-white rounded-2xl shadow-xl p-12 h-[400px] mt-32">
         <h1 className="text-2xl mb-6 font-semibold">Log In</h1>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="flex flex-col gap-1">
-            <label htmlFor="name">Email</label>
+            <label>Email</label>
             <input
+              {...register("email")}
               className="p-2 border-[1px] border-gray-600 rounded-md
             "
               type="text"
-              id="name"
-              name="name"
               placeholder="Enter your email"
             />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
           </div>
           <div className="mt-4 flex flex-col gap-1">
-            <label htmlFor="password">Password</label>
+            <label>Password</label>
             <input
+              {...register("password")}
               className="p-2 border-[1px] border-gray-600 rounded-md
             "
               type="password"
-              id="name"
-              name="name"
               placeholder="Enter your password"
             />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
           </div>
           <button
+            disabled={isSubmitting}
             className="bg-blue-300 mt-6 py-1 rounded-full px-12 mx-auto block hover:bg-blue-400"
             type="submit"
           >
-            Submit
+            {isSubmitting ? "Loggin in..." : "Submit"}
           </button>
         </form>
       </div>
