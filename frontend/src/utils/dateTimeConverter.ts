be146d583cc4951d47ba067feb34845d091088d7
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-09-01 18:32:12 +0100

    add 1 hour to dateString

--- frontend/src/utils/dateTimeConverter.ts
+++ frontend/src/utils/dateTimeConverter.ts
@@ -1,5 +1,6 @@
 export function convertDate(dateString: string) {
   const date = new Date(dateString);
+  date.setHours(date.getHours() + 1);
   const formatter = new Intl.DateTimeFormat("en-GB", {
     timeZone: "Europe/London",
     hour: "numeric",
